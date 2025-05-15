1cf394a78d56f202484300e45e3908badff992f5
yoinerdeyhan@gmail.com <yoinerdeyhan@gmail.com>
2025-05-14 22:03:44 -0500

    🐛 [fix] config sqlsrv

--- config/database.php
+++ config/database.php
@@ -108,8 +108,12 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => true,
+            'trust_server_certificate' => true,
+            'options' => [
+                PDO::SQLSRV_ATTR_ENCRYPT => 1,  // SQLSRV_ENCRYPT_ON
+                'TrustServerCertificate' => true,
+            ],
         ],
 
     ],
