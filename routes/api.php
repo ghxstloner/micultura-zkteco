4d3ec209fe7d3c272afa24d26570844ba2e77a45
yoinerdeyhan@gmail.com <yoinerdeyhan@gmail.com>
2025-05-16 19:37:12 -0500

    🚀 [deploy] token

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\TripulanteApiController;
 use Illuminate\Support\Facades\Route;
 
 // Rutas API para gestión de tripulantes y dispositivos ZKTeco
-Route::middleware('api-token')->group(function () {
+Route::middleware([\App\Http\Middleware\ApiTokenMiddleware::class])->group(function () {
     Route::post('/tripulantes', [TripulanteApiController::class, 'store']);
     Route::post('/tripulantes/sync-devices', [TripulanteApiController::class, 'syncDevices']);
     Route::post('/tripulantes/clear-devices', [TripulanteApiController::class, 'clearDevices']);
