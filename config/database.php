7b39f4495bd1d3fc283bb9d9aa7db93c9f8f0851
yoinerdeyhan@gmail.com <yoinerdeyhan@gmail.com>
2025-05-29 23:38:55 -0500

    🚑️ [hotfix] se eliminó referencia a sqlserver

--- config/database.php
+++ config/database.php
@@ -115,20 +115,6 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'options' => [],
-        ],
-
     ],
 
     /*
